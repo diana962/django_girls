6abf124262a8b37f386c36d973bb593cfa53e1f6
diana962 <fighterdiana@gmail.com>
2024-03-22 16:35:06 +0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'), #мы связали view под именем post_list с корневым URL-адресом ('')\
     # Последняя часть name='post_list' — это имя URL, которое будет использовано, чтобы идентифицировать его.
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
