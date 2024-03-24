358ed7174e5c699489f67c87bd3fccc501c3054f
diana962 <fighterdiana@gmail.com>
2024-03-25 00:19:06 +0600

    Ended

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'), #мы связали view под именем post_list с корневым URL-адресом ('')\
-    # Последняя часть name='post_list' — это имя URL, которое будет использовано, чтобы идентифицировать его.
+    path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -14,7 +14,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    if request.method == "POST":
+    if request.method == "POST": #это как метод GET
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
